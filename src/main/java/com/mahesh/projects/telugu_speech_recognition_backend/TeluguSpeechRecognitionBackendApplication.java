deda4e6ee913a7381b1ee4d5a05719fe6be9d046
umaalugubillli <maheshalugubilli@gmail.com>
2024-12-21 11:05:49 +0530

    Intial Commit - Project Creation

---
+++ src/main/java/com/mahesh/projects/telugu_speech_recognition_backend/TeluguSpeechRecognitionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.projects.telugu_speech_recognition_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeluguSpeechRecognitionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeluguSpeechRecognitionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahesh/projects/telugu_speech_recognition_backend/TeluguSpeechRecognitionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.projects.telugu_speech_recognition_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeluguSpeechRecognitionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
