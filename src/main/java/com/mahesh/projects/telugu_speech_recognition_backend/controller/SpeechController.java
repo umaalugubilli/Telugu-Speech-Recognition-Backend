fc9fb7ffe5faed9c0431bccc579888910443563b
umaalugubillli <maheshalugubilli@gmail.com>
2024-12-22 22:04:07 +0530

    Implemented Speech Prediction API with basic UI using JS.

--- src/main/java/com/mahesh/projects/telugu_speech_recognition_backend/controller/SpeechController.java
+++ src/main/java/com/mahesh/projects/telugu_speech_recognition_backend/controller/SpeechController.java
@@ -1,11 +1,47 @@
 package com.mahesh.projects.telugu_speech_recognition_backend.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.mahesh.projects.telugu_speech_recognition_backend.service.SpeechRecognitionService;
 
 @Controller
-@RequestMapping("/api")
 public class SpeechController {
 	
+	private SpeechRecognitionService speechRecognitionService;
+	
+	public SpeechController(SpeechRecognitionService speechRecognitionService) {
+		super();
+		this.speechRecognitionService = speechRecognitionService;
+	}
+	
+	
+	@PostMapping("/api/prediction")
+	public ResponseEntity<?> recognizeSpeech(
+			@RequestParam("file") MultipartFile file1
+			) throws IOException {
+		
+		String tempDir = System.getProperty("java.io.tmpdir");
+		Path tempFile = Paths.get(tempDir, UUID.randomUUID().toString() + ".wav");
+		
+		Files.copy(file1.getInputStream(), tempFile, StandardCopyOption.REPLACE_EXISTING);
+		
+		int digit = speechRecognitionService.recognizeDigit(tempFile.toString());
+		
+		Files.delete(tempFile);
+		
+		return ResponseEntity.ok(Map.of("result",digit));
+	}
 	
 }
--- src/main/java/com/mahesh/projects/telugu_speech_recognition_backend/controller/WelcomeController.java
+++ src/main/java/com/mahesh/projects/telugu_speech_recognition_backend/controller/WelcomeController.java
@@ -1,15 +1,24 @@
 package com.mahesh.projects.telugu_speech_recognition_backend.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 	
-	@GetMapping("/")
-	public String gotoWelcomePage(ModelMap model) {
-		return "/calculator.html";
+	@GetMapping("/calculate")
+	public String gotoWelcomePage(Model model) {
+		//model.addAttribute("pageTitle", "Speech Recognition Calculator");
+		return "calculator";
 	}
+
+	
+	@GetMapping("/predict")
+	public String gotoPredictionPage() {
+		return "prediction";
+	}
+	
+	
 
 }
